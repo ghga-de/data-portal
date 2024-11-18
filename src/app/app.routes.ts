a40e486b4f412e210f5110aa84d947f84e29c423
ac-jorellanaf <31940280+ac-jorellanaf@users.noreply.github.com>
2024-11-18 13:11:10 +0000

    Reformatted with Prettier. Disabled JSDOC for routes in-file.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,8 @@
+/* eslint-disable jsdoc/require-jsdoc */
 import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    /**
-     * Lazy-loading home page component
-     * @returns Lazy-loaded home page component
-     */
     loadComponent: () =>
       import('./portal/features/home-page/home-page.component').then(
         (m) => m.HomePageComponent,
@@ -13,10 +10,6 @@ export const routes: Routes = [
   },
   {
     path: 'browse',
-    /**
-     * Lazy-loading metadata browser component
-     * @returns Lazy-loaded metadata browser component
-     */
     loadComponent: () =>
       import('./metadata/features/metadata-browser/metadata-browser.component').then(
         (m) => m.MetadataBrowserComponent,
