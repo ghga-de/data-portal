d73f9ab88e7036e26ba5b71d79462c7b6df26cd7
ac-jorellanaf <31940280+ac-jorellanaf@users.noreply.github.com>
2024-12-04 12:11:46 +0000

    Initial commit

--- src/app/portal/features/home-page/home-page.component.ts
+++ src/app/portal/features/home-page/home-page.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 /**
  * This is the home page component
  */
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [MatIcon, MatButtonModule, RouterLink],
   templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {}
