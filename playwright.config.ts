f084254d82d3a7808bbb4ccfa9db5e70de0872bb
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2025-01-15 19:20:38 +0000

    Adapt webserver command for Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -79,7 +79,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'dev_launcher',
+    command: './run.js --dev',
     url: 'http://127.0.0.1:8080',
     reuseExistingServer: !process.env.CI,
   },
