1dd0e3c4a24e3098b7d73c454690d7a266802784
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2025-02-01 13:29:55 +0000

    Fix unit test

--- src/app/work-packages/features/work-package/work-package.component.spec.ts
+++ src/app/work-packages/features/work-package/work-package.component.spec.ts
@@ -4,9 +4,24 @@
  * @license Apache-2.0
  */
 
+import { signal } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { datasets } from '@app/../mocks/data';
+import { Dataset } from '@app/work-packages/models/dataset';
+import { WorkPackageService } from '@app/work-packages/services/work-package.service';
 import { WorkPackageComponent } from './work-package.component';
+
+import { screen } from '@testing-library/angular';
+
+/**
+ * Mock the work package service as needed for the work package component
+ */
+class MockWorkPackageService {
+  datasets = signal<Dataset[]>(datasets);
+  datasetsAreLoading = signal<boolean>(false);
+  datasetsError = signal<unknown>(undefined);
+}
 
 describe('WorkPackageComponent', () => {
   let component: WorkPackageComponent;
@@ -14,7 +29,8 @@ describe('WorkPackageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [WorkPackageComponent],
+      imports: [WorkPackageComponent, NoopAnimationsModule],
+      providers: [{ provide: WorkPackageService, useClass: MockWorkPackageService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(WorkPackageComponent);
@@ -24,5 +40,19 @@ describe('WorkPackageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show proper heading', () => {
+    const heading = screen.getByRole('heading');
+    expect(heading).toHaveTextContent('Download or upload datasets');
+  });
+
+  it('should show dataset selector', async () => {
+    const select = screen.getByLabelText('Available datasets');
+    expect(select).toBeTruthy();
+    select.click();
+    await fixture.whenStable();
+    const option = screen.getByText('GHGAD588887987: Some dataset to upload');
+    expect(option).toBeTruthy();
   });
 });
