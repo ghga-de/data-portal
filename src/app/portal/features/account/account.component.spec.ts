019e836d6e331a10ee79b0d4b1ef4a56f3b53435
Pascal Kraft <kraft.pascal@gmail.com>
2025-02-05 11:53:44 +0100

    fix unit test.

--- src/app/portal/features/account/account.component.spec.ts
+++ src/app/portal/features/account/account.component.spec.ts
@@ -15,6 +15,7 @@ import { AccountComponent } from './account.component';
 class MockAuthService {
   fullName = () => 'Dr. John Doe';
   email = () => 'j.jdoe@home.org';
+  role = () => 'data_steward';
 }
 
 describe('AccountComponent', () => {
