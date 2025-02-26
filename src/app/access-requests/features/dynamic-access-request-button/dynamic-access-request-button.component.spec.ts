b4f2da1be2ad9343c9d5b6d41aeb67f923f87751
Pascal Kraft <kraft.pascal@gmail.com>
2025-02-26 15:33:00 +0100

    Fix unit test.

--- src/app/access-requests/features/dynamic-access-request-button/dynamic-access-request-button.component.spec.ts
+++ src/app/access-requests/features/dynamic-access-request-button/dynamic-access-request-button.component.spec.ts
@@ -6,6 +6,8 @@
 
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ActivatedRoute } from '@angular/router';
+import { fakeActivatedRoute } from '@app/../mocks/route';
 import {
   AccessRequestService,
   MockAccessRequestService,
@@ -32,6 +34,7 @@ describe('DynamicAccessRequestButtonComponent', () => {
       imports: [DynamicAccessRequestButtonComponent],
       providers: [
         { provide: AccessRequestService, useClass: MockAccessRequestService },
+        { provide: ActivatedRoute, useValue: fakeActivatedRoute },
         { provide: AuthService, useClass: MockAuthService },
       ],
     }).compileComponents();
