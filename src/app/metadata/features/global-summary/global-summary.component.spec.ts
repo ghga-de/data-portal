f419ea608a7098b84bb55f242b163dc17f295be0
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2025-05-21 18:39:39 +0000

    Test aggregation in global stats

--- src/app/metadata/features/global-summary/global-summary.component.spec.ts
+++ src/app/metadata/features/global-summary/global-summary.component.spec.ts
@@ -44,10 +44,43 @@ describe('GlobalStatsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should show methods', () => {
+  it('should properly show total datasets', () => {
     const compiled = fixture.nativeElement as HTMLElement;
-    const text = compiled.textContent;
-    expect(text).toContain('700');
-    expect(text).toContain('Ilumina test');
+    const cards = compiled.querySelectorAll('mat-card');
+    expect(cards.length).toBe(4);
+    const card = cards[0];
+    const text = card.textContent?.replace(/\s+/g, ' ');
+    expect(text).toContain('Total datasets: 252');
+  });
+
+  it('should properly show experiments', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const cards = compiled.querySelectorAll('mat-card');
+    expect(cards.length).toBe(4);
+    const card = cards[1];
+    const text = card.textContent?.replace(/\s+/g, ' ');
+    expect(text).toContain('Experiments: 1,400');
+    expect(text).toContain('700 Ilumina test');
+    expect(text).toContain('700 HiSeq test');
+  });
+
+  it('should properly show individuals', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const cards = compiled.querySelectorAll('mat-card');
+    expect(cards.length).toBe(4);
+    const card = cards[2];
+    const text = card.textContent?.replace(/\s+/g, ' ');
+    expect(text).toContain('Individuals: 5,432');
+    expect(text).toContain('1,935 Female');
+    expect(text).toContain('2,358 Male');
+  });
+
+  it('should properly aggregate file types', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const cards = compiled.querySelectorAll('mat-card');
+    expect(cards.length).toBe(4);
+    const card = cards[3];
+    const text = card.textContent?.replace(/\s+/g, ' ');
+    expect(text).toContain('Files: 703 12 txt 462 bam 212 fastq 17 zip');
   });
 });
