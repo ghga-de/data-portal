ab38ff7d86ba280d7ecc24ecdf7a48e1b50e5f5d
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2025-05-21 18:59:27 +0000

    Further simplify tests

--- src/app/metadata/features/global-summary/global-summary.component.spec.ts
+++ src/app/metadata/features/global-summary/global-summary.component.spec.ts
@@ -40,45 +40,37 @@ describe('GlobalStatsComponent', () => {
     await fixture.whenStable();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
   /**
-   * Get the text content of a card
+   * Check the text content of a card
    * @param index the number of the card
-   * @returns the normalized text content of the card
+   * @param expected the text to check for
    */
-  function getCardText(index: number): string {
+  function expectCardText(index: number, expected: string): void {
     const compiled = fixture.nativeElement as HTMLElement;
     const cards = compiled.querySelectorAll('mat-card');
     expect(cards.length).toBe(4);
     const card = cards[index];
-    return card.textContent?.replace(/\s+/g, ' ') ?? '';
+    const text = card.textContent?.replace(/\s+/g, ' ') ?? '';
+    expect(text).toContain(expected);
   }
 
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
   it('should properly show total datasets', () => {
-    const text = getCardText(0);
-    expect(text).toContain('Total datasets: 252');
+    expectCardText(0, 'Total datasets: 252');
   });
 
   it('should properly show experiments', () => {
-    const text = getCardText(1);
-    expect(text).toContain('Experiments: 1,400');
-    expect(text).toContain('700 Ilumina test');
-    expect(text).toContain('700 HiSeq test');
+    expectCardText(1, 'Experiments: 1,400 700 Ilumina test 700 HiSeq test');
   });
 
   it('should properly show individuals', () => {
-    const text = getCardText(2);
-    expect(text).toContain('Individuals: 5,432');
-    expect(text).toContain('1,935 Female');
-    expect(text).toContain('2,358 Male');
+    expectCardText(2, 'Individuals: 5,432 1,935 Female 2,358 Male');
   });
 
   it('should properly aggregate file types', () => {
-    const text = getCardText(3);
-    expect(text).toContain('Files: 703');
-    expect(text).toContain('12 txt 462 bam 212 fastq 17 zip');
+    expectCardText(3, 'Files: 703 12 txt 462 bam 212 fastq 17 zip');
   });
 });
