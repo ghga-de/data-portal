83aeaf5140ecbf68d58e48aac4287ebb924be350
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2025-05-21 18:50:02 +0000

    Avoid redundant code in test

--- src/app/metadata/features/global-summary/global-summary.component.spec.ts
+++ src/app/metadata/features/global-summary/global-summary.component.spec.ts
@@ -44,43 +44,41 @@ describe('GlobalStatsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should properly show total datasets', () => {
+  /**
+   * Get the text content of a card
+   * @param index the number of the card
+   * @returns the normalized text content of the card
+   */
+  function getCardText(index: number): string {
     const compiled = fixture.nativeElement as HTMLElement;
     const cards = compiled.querySelectorAll('mat-card');
     expect(cards.length).toBe(4);
-    const card = cards[0];
-    const text = card.textContent?.replace(/\s+/g, ' ');
+    const card = cards[index];
+    return card.textContent?.replace(/\s+/g, ' ') ?? '';
+  }
+
+  it('should properly show total datasets', () => {
+    const text = getCardText(0);
     expect(text).toContain('Total datasets: 252');
   });
 
   it('should properly show experiments', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
-    const cards = compiled.querySelectorAll('mat-card');
-    expect(cards.length).toBe(4);
-    const card = cards[1];
-    const text = card.textContent?.replace(/\s+/g, ' ');
+    const text = getCardText(1);
     expect(text).toContain('Experiments: 1,400');
     expect(text).toContain('700 Ilumina test');
     expect(text).toContain('700 HiSeq test');
   });
 
   it('should properly show individuals', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
-    const cards = compiled.querySelectorAll('mat-card');
-    expect(cards.length).toBe(4);
-    const card = cards[2];
-    const text = card.textContent?.replace(/\s+/g, ' ');
+    const text = getCardText(2);
     expect(text).toContain('Individuals: 5,432');
     expect(text).toContain('1,935 Female');
     expect(text).toContain('2,358 Male');
   });
 
   it('should properly aggregate file types', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
-    const cards = compiled.querySelectorAll('mat-card');
-    expect(cards.length).toBe(4);
-    const card = cards[3];
-    const text = card.textContent?.replace(/\s+/g, ' ');
-    expect(text).toContain('Files: 703 12 txt 462 bam 212 fastq 17 zip');
+    const text = getCardText(3);
+    expect(text).toContain('Files: 703');
+    expect(text).toContain('12 txt 462 bam 212 fastq 17 zip');
   });
 });
