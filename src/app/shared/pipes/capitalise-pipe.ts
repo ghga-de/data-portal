4066b3b728937a5c5264b84b06c4dd1ac0097a6e
ac-jorellanaf <31940280+ac-jorellanaf@users.noreply.github.com>
2025-11-07 11:12:44 +0000

    Reverted changes to capitalise pipe and removed replace string pipe

--- src/app/shared/pipes/capitalise-pipe.ts
+++ src/app/shared/pipes/capitalise-pipe.ts
@@ -16,24 +16,9 @@ export class Capitalise implements PipeTransform {
   /**
    * This method will get a string and return it with its first letter capitalised.
    * @param str The string to capitalise
-   * @param lowercaseStartingString Whether we should convert the string to lowercase first. Default is false.
-   * @param capitaliseEveryWord Whether we should capitalise every word in the string. Default is false.
    * @returns The capitalised string
    */
-  transform(
-    str: string,
-    lowercaseStartingString: boolean = false,
-    capitaliseEveryWord: boolean = false,
-  ): string {
-    if (lowercaseStartingString) {
-      str = str.toLowerCase();
-    }
-    if (capitaliseEveryWord) {
-      return str
-        .split(' ')
-        .map((word) => word.substring(0, 1).toUpperCase() + word.substring(1))
-        .join(' ');
-    }
+  transform(str: string): string {
     return str.substring(0, 1).toUpperCase() + str.substring(1);
   }
 }
