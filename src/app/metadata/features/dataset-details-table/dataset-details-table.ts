fc612d8a1aab52ca453b5c7b786d8bcecad4b431
ac-jorellanaf <31940280+ac-jorellanaf@users.noreply.github.com>
2025-11-07 10:25:33 +0000

    Implemented improved hover mechanism

--- src/app/metadata/features/dataset-details-table/dataset-details-table.ts
+++ src/app/metadata/features/dataset-details-table/dataset-details-table.ts
@@ -63,24 +63,28 @@ export class DatasetDetailsTableComponent implements AfterViewInit {
     return `${header})`;
   });
 
-  protected isHovered = signal(false);
-
   protected tooltipDisabled = signal(false);
 
+  #hoverHeaderTime: number = 0;
+
   /**
-   * When a user hovers over the expansion panel, increment the hover count to hide the tooltip
+   * When entering the tooltip, memorize start time
    */
-  onHover(): void {
-    let countHovered = sessionStorage.getItem('CountDSDetailsTableHovered');
-    const numHovered: number = countHovered ? parseInt(countHovered) : 0;
-    sessionStorage.setItem('CountDSDetailsTableHovered', `${numHovered + 1}`);
-    setTimeout(() => {
-      if (numHovered > 2) {
-        this.tooltipDisabled.set(true);
-      } else {
-        this.tooltipDisabled.set(false);
-      }
-    }, 2000);
+  startHoverHeader(): void {
+    this.#hoverHeaderTime = Date.now();
+  }
+
+  /**
+   * After leaving the tooltip, disable if it was shown long enough
+   */
+  endHoverHeader(): void {
+    if (Date.now() - this.#hoverHeaderTime < 1000) return;
+    const key = `hint.${this.tableName()}.details.shown`;
+    if (!sessionStorage.getItem(key)) {
+      sessionStorage.setItem(key, 'true');
+      this.tooltipDisabled.set(true);
+    }
+    this.#hoverHeaderTime = 0;
   }
 
   protected numItems = computed(() => this.data().length);
@@ -130,5 +134,8 @@ export class DatasetDetailsTableComponent implements AfterViewInit {
     this.matPaginators.changes.subscribe(() => {
       if (this.paginator) this.dataSource.paginator = this.paginator;
     });
+
+    const key = `hint.${this.tableName()}.details.shown`;
+    if (sessionStorage.getItem(key)) this.tooltipDisabled.set(true);
   }
 }
