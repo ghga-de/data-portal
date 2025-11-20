8d5972399ba9e67d977d3a4a3599f98cd1e693c2
Jordy Orellana F. <31940280+ac-jorellanaf@users.noreply.github.com>
2025-11-20 17:16:15 +0000

    Added warnings for unverified and missing IVA for a grant

--- src/app/access-requests/features/active-access-grants-list/active-access-grants-list.ts
+++ src/app/access-requests/features/active-access-grants-list/active-access-grants-list.ts
@@ -5,13 +5,17 @@
  */
 
 import { DatePipe } from '@angular/common';
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterModule } from '@angular/router';
 import { AccessRequestService } from '@app/access-requests/services/access-request';
+import { AuthService } from '@app/auth/services/auth';
 import { FRIENDLY_DATE_FORMAT } from '@app/shared/utils/date-formats';
+import { Iva } from '@app/verification-addresses/models/iva';
+import { IvaService } from '@app/verification-addresses/services/iva';
 import { StencilComponent } from '../../../shared/ui/stencil/stencil/stencil';
+import { IvaTypePipe } from '@app/verification-addresses/pipes/iva-type-pipe';
 
 /**
  * This component shows a list of access grants that have been granted.
@@ -25,14 +29,35 @@ import { StencilComponent } from '../../../shared/ui/stencil/stencil/stencil';
     MatIconModule,
     MatButtonModule,
     RouterModule,
+    IvaTypePipe,
   ],
   templateUrl: './active-access-grants-list.html',
 })
-export class ActiveAccessGrantsListComponent {
+export class ActiveAccessGrantsListComponent implements OnInit {
   readonly friendlyDateFormat = FRIENDLY_DATE_FORMAT;
   #ars = inject(AccessRequestService);
+  #iva = inject(IvaService);
+  #auth = inject(AuthService);
 
   protected activeGrants = computed(() => this.#ars.activeUserAccessGrants());
-  protected isLoading = this.#ars.userAccessRequests.isLoading;
-  protected hasError = this.#ars.userAccessRequests.error;
+  protected isLoading = this.#ars.userAccessGrants.isLoading;
+  protected hasError = this.#ars.userAccessGrants.error;
+
+  #userId = computed<string | undefined>(() => this.#auth.user()?.id || undefined);
+
+  #userIvas = computed(() =>
+    this.#iva.userIvas.error() ? [] : this.#iva.userIvas.value(),
+  );
+
+  protected grantIvas = computed<(Iva | undefined)[]>(() =>
+    this.activeGrants().map((g) => this.#userIvas().find((iva) => iva.id === g.iva_id)),
+  );
+
+  protected anyActiveGrantWithValidIva = computed(() =>
+    this.grantIvas().some((iva) => iva?.state === 'Verified'),
+  );
+
+  ngOnInit() {
+    this.#iva.loadUserIvas(this.#userId());
+  }
 }
