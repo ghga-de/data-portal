3a1e4f768b68d08426921df1b947d8f39b4d4317
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2026-03-10 10:48:56 +0000

    Add UploadGrantBase interface

--- src/app/upload/models/grant.ts
+++ src/app/upload/models/grant.ts
@@ -4,22 +4,26 @@
  * @license Apache-2.0
  */
 
-/** An upload access grant. */
-export interface UploadGrant {
-  /** Internal grant ID (same as claim ID) */
-  id: string;
+/** Base data to create an upload grant */
+export interface UploadGrantBase {
   /** Internal user ID */
   user_id: string;
   /** ID of an IVA associated with this grant */
   iva_id: string | null;
   /** ID of the upload box this grant is for */
   box_id: string;
-  /** Date of creation of this grant */
-  created: string;
   /** Start date of validity */
   valid_from: string;
   /** End date of validity */
   valid_until: string;
+}
+
+/** An upload access grant */
+export interface UploadGrant extends UploadGrantBase {
+  /** Internal grant ID (same as claim ID) */
+  id: string;
+  /** Date of creation of this grant */
+  created: string;
   /** Full name of the user */
   user_name: string;
   /** The email address of the user */
@@ -28,7 +32,7 @@ export interface UploadGrant {
   user_title: string | null;
 }
 
-/** An UploadGrant with the ResearchDataUploadBox title and description. */
+/** An UploadGrant with the ResearchDataUploadBox title and description */
 export interface GrantWithBoxInfo extends UploadGrant {
   /** Short meaningful name for the box */
   box_title: string;
