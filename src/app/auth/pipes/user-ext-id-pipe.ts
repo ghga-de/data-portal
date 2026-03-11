dee704e8dc0c5b599e6e84302fd2089e9cb043ed
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2026-03-11 14:45:17 +0000

    Completely relocate user ext id pipe

--- src/app/auth/pipes/user-ext-id-pipe.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Re-exports UserExtIdPipe from shared for backward compatibility
- * @copyright The GHGA Authors
- * @license Apache-2.0
- */
-
-export { UserExtIdPipe } from '@app/shared/pipes/user-ext-id-pipe';
